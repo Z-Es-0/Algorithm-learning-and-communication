de614d8f152300e1b8cea6fb94532da51738f16c
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-01-23 22:39:47 +0800

    orz

--- 算法/埃拉托斯特尼筛法，选素数.py
+++
@@ -1,9 +0,0 @@
-
-#埃拉托斯特尼筛法（Sieve of Eratosthenes）
-l=1000001#数据范围
-c=[1,1]+[0]*l
-for i in range(2,l):
-    if 1-c[i]:
-        for j in range(i*i,l,i):
-            c[j]=1
-print(c)#c数组是一个以0表示素数，以1表示非素数的数组
---
+++ 算法/数论/算数基本定理.py
@@ -0,0 +1,14 @@
+def prime_factors(n):
+    '''算数基本定理
+    参数n，返回值为将n拆分的数组
+    即该数组的乘积等于n'''
+    factors = []
+    divisor = 2
+    while divisor <= n:
+        if n % divisor == 0:
+            factors.append(divisor)
+            n = n // divisor
+        else:
+            divisor += 1
+
+    return factors#返回值为数组
