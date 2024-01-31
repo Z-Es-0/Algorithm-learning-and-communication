be10b9e1872ca595ea097f17fbb2dd39fcbd2ae7
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-01-31 18:56:52 +0800

    Create 前缀和，差分.py

---
+++ 算法/小寄巧/前缀和，差分.py
@@ -0,0 +1,22 @@
+s=[2,3,8,9,1,3,56,7,8,3,4]
+g=[]#前缀和数组
+o=0
+for i in s:
+    o+=i
+    g.append(o)
+print(g)
+
+lst=s[0]
+diff=[s[0]]
+for i in range(1,len(s)):
+    diff.append(s[i]-lst)
+print(diff)
+
+def cafen(s):
+    lst=s[0]
+    diff=[s[0]]
+    for i in range(1,len(s)):
+        diff.append(s[i]-lst)
+        lst=s[i]
+    return diff
+print(cafen(g))#前缀和差分回去
