415df7bc7cb3b2124c1a5f4db385e4ec086a7277
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-02-10 13:41:09 +0800

    Create 全能进制转换.py

---
+++ 算法/数论/全能进制转换.py
@@ -0,0 +1,25 @@
+f=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+def cse(n, base):
+    if n == 0:
+        return "0"
+
+    result = ""
+    while n != 0:
+        r = n % base
+        # print(n, base, r)
+        n //= base
+
+
+        if r < 0:
+            r += abs(base)
+            n += 1
+
+        result = f[r] + result
+
+    return result
+
+
+if __name__ == "__main__":
+    n,b=map(int,input().split())#输入的n为10进制数字，b为进制基数
+    result = cse(n, b)
+    print(f"{n}={result}(base{b})")
