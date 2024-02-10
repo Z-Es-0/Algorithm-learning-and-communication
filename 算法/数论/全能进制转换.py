aae26122d3c5017b832c996a416fe1706ffd2f5b
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-02-10 13:51:05 +0800

    orz

--- 算法/数论/全能进制转换.py
+++ 算法/数论/全能进制转换.py
@@ -1,14 +1,14 @@
 f=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 def cse(n, base):
+    global f
     if n == 0:
         return "0"
 
     result = ""
     while n != 0:
         r = n % base
-        # print(n, base, r)
-        n //= base
 
+        n //= base
 
         if r < 0:
             r += abs(base)
