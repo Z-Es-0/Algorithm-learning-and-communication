025a244758c0877e222e13c7bd9c39ac2276b11d
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-07-18 09:36:54 +0800

    man!

---
+++ 算法模板cpp/单调队列+k窗口.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+//维护固定长度为k区间的区间最小值；
+class MonotonicQueue {
+public:
+    void push(int val) {
+        while (!dq.empty() && dq.back() > val) {
+            dq.pop_back();
+        }
+        dq.push_back(val);
+    }
+
+    void pop(int val) {
+        if (!dq.empty() && dq.front() == val) {
+            dq.pop_front();
+        }
+    }
+
+    int min() const {
+        return dq.front();
+    }
+
+    void clear() {
+        dq.clear();
+    }
+
+private:
+    deque<int> dq;
+};
+
+class SlidingWindow {
+public:
+    SlidingWindow(int k) : k(k) {}
+
+    void add(int val) {
+        if (window.size() >= k) {
+            mq.pop(window.front());
+            window.pop_front();
+        }
+        window.push_back(val);
+        mq.push(val);
+    }
+
+    int getMin() const {
+        return mq.min();
+    }
+
+    void clear() {
+        window.clear();
+        mq.clear();
+    }
+
+private:
+    int k;
+    deque<int> window;
+    MonotonicQueue mq;
+};
