36ea0e6fa7e567174ec5dba8c316b51168220976
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-07-19 07:46:08 +0800

    1

---
+++ 算法模板cpp/树上找爹.cpp
@@ -0,0 +1,8 @@
+void finds(int node, int parent, const map<int, vector<int>>& adj, map<int, int>& parents) {
+    parents[node] = parent;
+    for (int neighbor : adj.at(node)) {
+        if (neighbor != parent) {
+            finds(neighbor, node, adj, parents);
+        }
+    }
+}
