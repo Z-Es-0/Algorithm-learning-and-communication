7e9a09e09b496bede097cb91f2897b05ec4c9b84
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-08-03 20:58:27 +0800

    updata

---
+++ 算法模板cpp/并查集.cpp
@@ -0,0 +1,85 @@
+/*
+ * 
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃ 　
+ * 　┃　　　━　　　┃ ++ + + +
+ *  ████━████ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃　　　　　　　　　　　
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug　　
+ * 　　　┃　　　┃　　+　　　　　　　　　
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * 
+ */
+
+
+
+/*
+ * @Author: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
+ * @Date: 2024-08-03 20:45:20
+ * @LastEditors: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
+ * @LastEditTime: 2024-08-03 20:52:24
+ * @FilePath: \Algorithm-learning-and-communication\算法模板cpp\并查集.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <vector>
+#include <numeric> // for std::iota
+
+class UnionFind {
+private:
+    std::vector<int> parent;  // 用于存储每个元素的父节点
+    std::vector<int> rank;    // 用于存储每个元素的秩（树的高度）
+    int count;                // 用于存储家族的数量
+
+public:
+    // 构造函数，初始化并查集
+    UnionFind(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        std::iota(parent.begin(), parent.end(), 0); // 初始化每个元素的父节点为自己
+        count = n; // 初始时，每个元素都是一个独立的家族
+    }
+
+    // 查找元素的根节点，并进行路径压缩
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // 路径压缩
+        }
+        return parent[x];
+    }
+
+    // 合并两个元素所属的家族
+    void union_sets(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+            count--; // 每次合并，家族数量减一
+        }
+    }
+
+    // 返回当前家族的数量
+    int getCount() const {
+        return count;
+    }
+};
--- 算法模板cpp/拓扑排序vv.cpp
+++ 算法模板cpp/拓扑排序vv.cpp
@@ -1,3 +1,12 @@
+/*
+ * @Author: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
+ * @Date: 2024-07-31 15:40:09
+ * @LastEditors: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
+ * @LastEditTime: 2024-08-03 20:54:19
+ * @FilePath: \Algorithm-learning-and-communication\算法模板cpp\拓扑排序vv.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
 vector<int> topologicalSort(const vector<vector<int>>& adjList) { //v<v<i>>表示邻接表，即father ->son;
     int n = adjList.size();
     vector<int> indegree(n, 0);
