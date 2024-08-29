6ffe0bab76fd290fb64f2441c3deabf79a5174d3
Z-Es-0 <141395766+Z-Es-0@users.noreply.github.com>
2024-08-29 19:40:40 +0800

    1

--- 算法模板cpp/归并排序.cpp
+++ 算法模板cpp/归并排序.cpp
@@ -2,7 +2,7 @@
  * @Author: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
  * @Date: 2024-08-27 15:20:15
  * @LastEditors: Z-Es-0 141395766+Z-Es-0@users.noreply.github.com
- * @LastEditTime: 2024-08-27 15:23:04
+ * @LastEditTime: 2024-08-27 19:29:50
  * @FilePath: \Algorithm-learning-and-communication\算法模板cpp\归并排序.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,17 +13,9 @@ using namespace std;
 #define N 100005
 #define MOD (1e9+7)
 int t,n;
-void merge_sort(vector<int>vec){
-    
-}
+
 void solve(){
-    vector<int>show ={9,2,3,8,6,1,7};
-    merge_sort(show);
-    for (int i = 0; i < show.size(); i++)
-    {
-        cout<<show[i]<<' ';
-    }
-    cout<<endl;
+      
 }
 signed main(){
 ios::sync_with_stdio(false);
