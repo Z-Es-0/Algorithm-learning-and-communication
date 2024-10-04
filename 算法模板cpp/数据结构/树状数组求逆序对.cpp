05fab61fd756c374f68f1aa103365a4b299e226b
Z-Es-0 <zes18642300628@qq.com>
2024-10-04 15:19:40 +0800

    Update 树状数组求逆序对.cpp

--- 算法模板cpp/数据结构/树状数组求逆序对.cpp
+++ 算法模板cpp/数据结构/树状数组求逆序对.cpp
@@ -1,3 +1,13 @@
+/*
+ * @Author: Z-Es-0 zes18642300628@qq.com
+ * @Date: 2024-07-26 15:03:19
+ * @LastEditors: Z-Es-0 zes18642300628@qq.com
+ * @LastEditTime: 2024-10-03 15:07:49
+ * @FilePath: \Algorithm-learning-and-communication\算法模板cpp\数据结构\树状数组求逆序对.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
 #define lowbit(x) (x & -x)
 #define N 200005
 #define MOD 1000000007
